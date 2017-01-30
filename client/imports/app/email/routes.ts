3de298a9a4acd397918ff25292ed2db99f367513
Rahul Sethi <rahulse.sdei@gmail.com>
2017-01-30 19:36:42 +0530

    added access control security while updating emails

--- client/imports/app/email/routes.ts
+++ client/imports/app/email/routes.ts
@@ -1,12 +1,14 @@
 import { Route } from '@angular/router';
 import { Meteor } from 'meteor/meteor';
+import { AuthService } from "../../services/auth";
+
 import { CreateEmailComponent } from "./create";
 import { ListEmailComponent } from "./list";
 
 
 export const routes = [
-    {path: "email/create", component: CreateEmailComponent},
-    {path: "email/update/:id", component: CreateEmailComponent},
-    {path: "email/list", component: ListEmailComponent},
+    {path: "email/create", component: CreateEmailComponent, canActivate: [AuthService], data: {'roles': ['super-admin']}},
+    {path: "email/update/:id", component: CreateEmailComponent, canActivate: [AuthService], data: {'roles': ['super-admin']}},
+    {path: "email/list", component: ListEmailComponent, canActivate: [AuthService], data: {'roles': ['super-admin']}},
 ];
 
