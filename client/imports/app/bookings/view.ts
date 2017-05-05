b1111a3cd0f011729dc38576379c1b61e64e9c10
Mihir Jain <mihir.jain@smartdatainc.net>
2017-05-05 15:01:05 +0530

    listed cancelled booking in booking list and view booking

--- client/imports/app/bookings/view.ts
+++ client/imports/app/bookings/view.ts
@@ -50,4 +50,25 @@ export class ViewBookingComponent extends MeteorComponent {
 
     this.error = '';
   }
+
+  getBookingStatus(item) {
+    let retVal = null;
+
+    // check completed flag
+    if (new Date(item.startDate) < new Date()) {
+      item.completed = true;
+    }
+
+    if (item.confirmed !== true && item.cancelled !== true) {
+        retVal = "Pending";
+    } else if (item.confirmed === true && item.completed !== true) {
+        retVal = "Confirmed";
+    } else if (item.completed === true) {
+        retVal = "Completed";
+    } else if (item.confirmed !== true && item.cancelled === true) {
+      retVal = "Cancelled";
+    }
+
+    return retVal;
+  }
 }
