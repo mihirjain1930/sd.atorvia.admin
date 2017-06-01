af0e6673e07e15a415806ef9a9139ea1596a195c
Mihir Jain <mihir.jain@smartdatainc.net>
2017-06-01 18:48:53 +0530

    changed thumbnail of all images according to their size

--- server/main.ts
+++ server/main.ts
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 import './startup/accounts-config.js';
 import './imports/publications/users';
+import './imports/routers/customer.routes';
 import './imports/routers/paypal.routes.ts';
 
 Meteor.startup(() => {
